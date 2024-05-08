dada77ccb15197d40226955266def0cee1c0e61d
anik558363 <anikmondol558363@gmil.com>
2024-05-08 18:26:55 +0600

    update my projects.and added this code in github

--- js/utility.js
+++ js/utility.js
@@ -1,11 +1,4 @@
-function showElementById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.remove('hidden');
-}
-function hideElementById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('hidden');
-}
+
 
 // -------------------------------
 function addDisabled(elementId){
